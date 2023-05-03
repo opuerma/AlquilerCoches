f494314d8dfab239243e4fa415261ca6bfe27e48
Oscar <opuerma@gmail.com>
2023-05-03 17:38:27 +0200

    Hechas secciones 'Nosotros' y 'Contacto'

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
