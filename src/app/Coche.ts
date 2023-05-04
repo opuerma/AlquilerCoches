4b9c4b7c0b19b93308e68f902d84d217a8f9b1d9
Oscar <opuerma@gmail.com>
2023-05-04 13:39:00 +0200

    Hecha interface, añadido coches y hecho componente Coches - falta responsive

---
+++ src/app/Coche.ts
@@ -0,0 +1,10 @@
+
+export interface Coche {
+    marca: string;
+    modelo: string;
+    autonomia: number;
+    puertas: number;
+    embrague: boolean;
+    precio: number;
+    descripcion: string;
+}
